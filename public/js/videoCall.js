3ddd9c31725fbc48dd63d7b57e5fdb1747465cbf
hyperloop11 <shirin.kaul11@gmail.com>
2021-07-12 03:02:24 +0530

    fixed peer server

--- public/js/videoCall.js
+++ public/js/videoCall.js
@@ -9,6 +9,7 @@ const socket = io('/');
 const username = meetUsername
 
 var peer = new Peer(undefined, {
+    secure: true,
     host: 'protected-retreat-21863.herokuapp.com',
     port: '443'
     // setting port for peerjs to listen on
